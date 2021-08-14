c57f49e34b5ab1de705ff472eca262c85e05d8ab
Meshari Aldoukhi <mashry151@hotmail.com>
2021-08-14 23:03:14 +0300

    form

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 /* Global Library */
 import { Switch, Route } from 'react-router'
 import Register from './components/authentication/Register';
+import Login from './components/authentication/Login';
 
 function App() {
   return (
     <Switch>
       <Route path="/register">
         <Register />
+      </Route>
+      <Route path="/login">
+        <Login />
       </Route>
     </Switch>
   );
--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -1,11 +1,54 @@
 import React from 'react';
-import { GlobalStyle } from './styles';
+import { CoverImage } from './styles';
+
+/* Routes Library */
+import { Link } from "react-router-dom"
 
 function Login() {
     return (
-        <div>
-            <GlobalStyle />
-            <h1>test</h1>
+        <div className="d-md-flex h-md-100 align-items-center">
+            {/* Left Side */}
+            <div className="col-md-6 p-0  h-md-100">
+                <CoverImage className="d-md-flex align-items-center h-100 p-5 text-center justify-content-center"></CoverImage>
+            </div>
+
+            {/* Right Side */}
+            <div className="col-md-6 p-0 bg-white h-md-100 loginarea">
+                <div className="align-items-center h-md-100 p-5 justify-content-center">
+                    {/* Company Logo */}
+                    <div className="brand">
+                        <img />
+                        LOGO
+                    </div>
+
+                    {/* Title Form */}
+                    <p className="text-center">Sign into your account</p>
+
+                    {/* Register Form */}
+                    <form>
+                        <div className="row g-3 mb-3">
+                            <div class="col">
+                                <input
+                                    type="text"
+                                    class="form-control"
+                                    placeholder="Username"
+                                />
+                            </div>
+                        </div>
+                        <div className="mb-3">
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="Password"
+                            />
+                        </div>
+                        <button type="submit" className="btn btn-dark w-100 d-grid mx-auto">
+                            Login
+                        </button>
+                    </form>
+                    <Link to="/register">Don't have an account? Register here</Link>
+                </div>
+            </div>
         </div>
     );
 }
