a2f14612e7f5741c1725cecb7d23ebba48431b9c
Meshari Aldoukhi <mashry151@hotmail.com>
2021-08-16 20:53:40 +0300

    register

--- src/components/authentication/Register.js
+++ src/components/authentication/Register.js
@@ -1,20 +1,37 @@
-import React from "react";
+/* Libraries */
+import React, { useState } from "react";
 import { observer } from "mobx-react";
 
 //import { Redirect } from 'react-router-dom';
 
-
+/* Store */
+import authStore from "../../stores/authStore"
 /* Styles */
 import { CoverImage, Logo } from "./styles";
 import LoGo from "../../media/LOGO.svg";
 
 
 function Register() {
-    /* Submit handler */
-    const handleSubmit = () => {
-        /* for testing */
+    /* New State for username */
+    const [user, setUser] = useState({
+        username: "",
+        password: "",
+        email: "",
+        phoneNumber: "",
+    })
+    /* handle the change of all inputs */
+    const handleChange = (event) => {
+        setUser({ ...user, [event.target.name]: event.target.value })
     }
-    /* will check if the path is working or not */
+
+    /* Submit handler */
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        /* check if we add it in stores or in the register side */
+        user.userType = "company"
+        authStore.register(user)
+    }
+    /* redirect to profile page */
     //if (authStore.user) return <Redirect to="/:username/contractList" />;
     return (
         <div className="d-md-flex h-md-100 align-items-center">
@@ -40,33 +57,41 @@ function Register() {
                         <div className="row g-3 mb-3">
                             <div className="col-sm-12 col-md-12 col-lg-6">
                                 <input
+                                    name="username"
                                     type="text"
                                     className="form-control"
                                     placeholder="Username"
+                                    onChange={handleChange}
                                 />
                             </div>
                             <div className="col-sm-12 col-md-12 col-lg-6">
                                 <input
+                                    name="password"
                                     type="password"
                                     className="form-control"
                                     placeholder="Password"
                                     autoComplete="password"
+                                    onChange={handleChange}
                                 />
                             </div>
                         </div>
                         <div className="row g-3 mb-3">
                             <div className="col-sm-12 col-md-12 col-lg-6">
                                 <input
+                                    name="email"
                                     type="email"
                                     className="form-control"
                                     placeholder="Email"
+                                    onChange={handleChange}
                                 />
                             </div>
                             <div className="col-sm-12 col-md-12 col-lg-6">
                                 <input
+                                    name="phoneNumber"
                                     type="tel"
                                     className="form-control"
                                     placeholder="Phone Number"
+                                    onChange={handleChange}
                                 />
                             </div>
                         </div>
