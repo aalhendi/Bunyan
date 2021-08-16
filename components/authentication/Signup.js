076d0c84f2e25e5270efe52d0503dbe3630302bc
Mohammad Alzamami <alzamami981@gmail.com>
2021-08-16 18:35:15 +0300

    Signin & Signup

--- components/authentication/Signup.js
+++ components/authentication/Signup.js
@@ -2,27 +2,30 @@ import React, { useState } from "react";
 import { Input, Icon, Center, Box, Button } from "native-base";
 import { Ionicons } from "@expo/vector-icons";
 import { View } from "react-native";
-import { AuthBackground, AuthButtonText, AuthOther, AuthTitle } from "./styles";
+import {
+  AuthBackground,
+  AuthButtonText,
+  AuthOther,
+  AuthTitle,
+  AuthWhiteBackground,
+} from "./styles";
 import { Entypo } from "@expo/vector-icons";
 
 const Singup = ({ navigation }) => {
   const [show, setShow] = useState(false);
   const handleClick = () => setShow(!show);
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ flex: 9 }}>
-        <AuthBackground
-          source={{
-            uri: "https://previews.123rf.com/images/microone/microone1809/microone180900880/107816017-building-house-work-process-of-buildings-construction-and-machinery-flat-vector-concept-development-.jpg",
-          }}
-        />
-      </View>
-
-      <View style={{ flex: 10, marginTop: "5%" }}>
-        <Center>
+    <AuthBackground
+      blurRadius={12.5}
+      source={{
+        uri: "https://previews.123rf.com/images/microone/microone1809/microone180900880/107816017-building-house-work-process-of-buildings-construction-and-machinery-flat-vector-concept-development-.jpg",
+      }}
+    >
+      <View style={{ flex: 5, marginTop: "5%" }}>
+        <Center style={{ flex: 1 }}>
           <AuthTitle>Sign up</AuthTitle>
           <Box w="80%">
-            <View style={{ marginBottom: 15 }}>
+            <View style={{ marginBottom: 10 }}>
               <Input
                 InputLeftElement={
                   <Icon as={<Ionicons name="person" />} size="md" m={2} />
@@ -30,7 +33,7 @@ const Singup = ({ navigation }) => {
                 placeholder="username" // mx={4}
               />
             </View>
-            <View style={{ marginBottom: 15 }}>
+            <View style={{ marginBottom: 10 }}>
               <Input
                 type={show ? "text" : "password"}
                 InputLeftElement={
@@ -49,7 +52,7 @@ const Singup = ({ navigation }) => {
                 placeholder="password" // mx={4}
               />
             </View>
-            <View style={{ marginBottom: 15 }}>
+            <View style={{ marginBottom: 10 }}>
               <Input
                 InputLeftElement={
                   <Icon as={<Entypo name="email" />} size="md" m={2} />
@@ -57,17 +60,15 @@ const Singup = ({ navigation }) => {
                 placeholder="email" // mx={4}
               />
             </View>
-            <Button> Sign up</Button>
+            <Button>Sign up</Button>
           </Box>
-          <View>
-            <AuthOther>already have an account?</AuthOther>
-            <AuthButtonText onPress={() => navigation.navigate("Signin")}>
-              Sign in
-            </AuthButtonText>
-          </View>
+          <AuthOther>already have an account?</AuthOther>
+          <AuthButtonText onPress={() => navigation.navigate("Signin")}>
+            Sign in
+          </AuthButtonText>
         </Center>
       </View>
-    </View>
+    </AuthBackground>
   );
 };
 
--- components/authentication/styles.js
+++ components/authentication/styles.js
@@ -2,21 +2,26 @@ import styled from "styled-components/native";
 
 export const AuthTitle = styled.Text`
   color: black;
-  font-size: 24px;
+  font-size: 25px;
   margin-bottom: 5%;
-  border-bottom-color: black;
+  font-weight: bold;
 `;
 export const AuthButtonText = styled.Text`
-  color: #5588a3;
+  color: white;
   font-weight: bold;
-  font-size: 18px;
+  font-size: 20px;
 `;
 export const AuthOther = styled.Text`
-  color: grey;
+  color: white;
   margin-top: 4%;
 `;
 
 export const AuthBackground = styled.ImageBackground`
   flex: 1;
   width: 100%;
+`;
+export const AuthWhiteBackground = styled.ImageBackground`
+  width: 100%;
+  height: 50%;
+  opacity: 0.5;
 `;
