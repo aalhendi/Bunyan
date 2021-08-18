8fbd54e55faae00458bb3f5ff818f71137ef6669
aalhendi <abdulrazaq.alhendi@gmail.com>
2021-08-18 20:11:29 +0300

    Delete unused routes

--- API/worker/routes.js
+++ API/worker/routes.js
@@ -1,31 +1,24 @@
 /* Imports */
 const express = require("express");
-const passport = require("passport");
-const { upload } = require("../../middleware/multer");
 
 /* Route Imports */
-const {
-  fetchTest,
-  routeTest,
-  uploadImage,
-  deleteImage,
-  fetchWorkers,
-} = require("./controllers");
+const { fetchWorkers } = require("./controllers");
 
 const router = express.Router();
 
 /* Params Middleware */
-router.param("testId", async (req, res, next, testId) => {
-  const test = await fetchTest(testId, next);
-  if (test) {
-    req.test = test;
+router.param("workerId", async (req, res, next, workerId) => {
+  const worker = await fetchWorker(workerId, next);
+  if (worker) {
+    req.worker = worker;
     next();
   } else {
-    const error = new Error("Test Object Not Found.");
+    const error = new Error("Worker Object Not Found.");
     error.status = 404;
     next(error);
   }
 });
+
 /* Fetch Workers*/
 router.get("/", fetchWorkers);
 
