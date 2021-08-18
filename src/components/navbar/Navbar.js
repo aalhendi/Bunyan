48d0c50c23a0944b0fd82e877b62f53d9f61f516
Meshari Aldoukhi <mashry151@hotmail.com>
2021-08-18 19:15:06 +0300

    Logout

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import authStore from "../../stores/authStore";
 import { LinkNav, Nav } from "./styles";
+import { BiLogOut } from 'react-icons/bi';
 
 function Navbar() {
   /* logout and redirect to login page */
@@ -9,7 +10,7 @@ function Navbar() {
       <div className="container-fluid ">
         <i className="navbar-brand">Navbar</i>
         <div className="collapse navbar-collapse">
-          <ul className="navbar-nav align-items-center">
+          <ul className="navbar-nav align-items-center w-100">
             <li className="nav-item">
               <LinkNav className="nav-link" exact to="/">
                 Home
@@ -26,13 +27,13 @@ function Navbar() {
               </LinkNav>
             </li>
             {/* Check if possible to have logout  and link to login*/}
-            <li className="nav-item">
+            <li className="nav-item ms-auto">
               <LinkNav
                 onClick={() => authStore.logout()}
                 to="/login"
-                className="float-end"
+                className=""
               >
-                Logout
+                <BiLogOut style={{ fontSize: 22 }} />
               </LinkNav>
             </li>
           </ul>
