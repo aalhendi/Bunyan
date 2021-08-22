da8a37f20d05c97f2fd61ec934504052e2495db7
Mohamed Youssef <mohamedyoussef@Mohameds-MacBook-Pro.local>
2021-08-22 14:24:28 +0300

    update Task

--- API/task/routes.js
+++ API/task/routes.js
@@ -1,8 +1,9 @@
 /* Imports */
 const express = require("express");
+const passport = require("passport");
 
 /* Route Imports */
-const { fetchTasks, addTask } = require("./controllers");
+const { fetchTasks, addTask, updateTask, fetchTask } = require("./controllers");
 
 const router = express.Router();
 
@@ -22,5 +23,11 @@ router.param("taskId", async (req, res, next, taskId) => {
 /* Fetch Task*/
 router.get("/", fetchTasks);
 router.post("/", addTask);
+/* Update Task status  */
+router.put(
+  "/:taskId",
+  passport.authenticate("jwt", { session: false }),
+  updateTask
+);
 
 module.exports = router;
